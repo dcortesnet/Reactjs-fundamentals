6bc1c9f42b5df25c9fdff7f9d3e63b9f3a1fe1e3
dcortesnet <dcortes.net@gmail.com>
2024-01-18 12:28:12 -0300

    Delete exercises

--- 06. context/src/01. context/provider-context.jsx
+++ 06. context/src/01. context/provider-context.jsx
@@ -3,7 +3,7 @@ import { myContext } from "./context";
 import { ConsumerContextComponent } from "./consumer-context";
 
 export function ProviderContextComponent() {
-  const sharedData = "My shared data!";
+  const sharedData = "My shared data!!!";
 
   return (
     <myContext.Provider value={sharedData}>
